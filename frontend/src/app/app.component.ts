e847552571f29754d515d08fc1cbf4a42f4e065a
Martin Pultz <mtpultz@gmail.com>
2022-07-30 22:02:24 -0600

    Check failure occurs on linting

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public title = 'frontend';
+  title = 'frontend';
 
   public constructor() {}
 }
