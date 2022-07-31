9623ac6fa82421a3c13ab0f3fe0d42a4418edde0
Martin Pultz <mtpultz@gmail.com>
2022-07-30 22:07:39 -0600

    Added nestjs test workflow

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(test).toBeTruthy();
-    expect(app).toBeFalsy();
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'frontend'`, () => {
